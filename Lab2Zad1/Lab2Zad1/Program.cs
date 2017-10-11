69f3a51359f4b7f27c2c864c4c4739c2f46e0dca
Michał Marasek <mimatek22@gmail.com>
2017-10-11 13:57:02 +0200

    dodano siemanko

--- Lab2Zad1/Lab2Zad1/Program.cs
+++ Lab2Zad1/Lab2Zad1/Program.cs
@@ -7,6 +7,7 @@ namespace Lab2Zad1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("siemanko!");
             string s = Console.ReadLine();
             Console.WriteLine(s);
             Console.ReadLine();
